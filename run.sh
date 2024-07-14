2de60f3ff6efcb28f08e75bff4b88b4eaaf16d3d
bbz <2632141215@qq.com>
2024-07-14 17:07:10 +0800

    test: jenkins

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ echo "collect conf done"
 # scp
 echo "scp ..."
 #ping -c 1 node2 || exit 1
-scp -r /data/etc root@node1:/data/etc || { echo "Failed to scp files"; exit 1; }
+scp -r /data/etc/* root@node1:/data/etc/ || { echo "Failed to scp files"; exit 1; }
 #scp -r /data/etc root@node2:/data/etc || { echo "Failed to scp files"; exit 1; } # 无限循环
 echo "scp done ..."
 
