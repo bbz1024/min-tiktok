d7cc7b3471532a920ee7bd150e4290c7996f7dd9
bbz <2632141215@qq.com>
2024-07-14 19:01:33 +0800

    test: jenkins

--- Dockerfile
+++ Dockerfile
@@ -6,18 +6,19 @@ ENV CGO_ENABLED 0
 ENV GOPROXY https://goproxy.cn/,direct
 
 COPY . .
-
+RUN tar -xvf ./pkg/upx.4.2.4.tar.xz
+RUN mv ./pkg/upx.4.2.4/upx upx
 RUN go mod download
 RUN go mod tidy
-RUN bash ./scripts/build-all.sh
+RUN bash ./scripts/build.sh
 
 FROM alpine:3.19
 RUN apk update && apk add tzdata # 时区 https://segmentfault.com/a/1190000040524996
-ENV TZ Asia/Shanghai  # panic: unknown time zone Asia/Shanghai
+ENV TZ Asia/Shanghai  #panic: unknown time zone Asia/Shanghai
 
 WORKDIR /project
 
 COPY --from=builder /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 COPY --from=builder  /output .
-
+COPY --from=builder  /build/scripts/run.sh .
 CMD ["top"]
---
+++ scripts/run.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+function clear_and_run() {
+    local name=$1
+    local type=$2
+
+    # Capitalize the first letter of the name
+    local capName="${name^}"
+
+    # Check if the type is 'api'
+    if [ "$type" == "api" ]; then
+        local capNameApi="${capName}Api"
+        echo "clearing api"
+        pushd api || exit
+        # Loop through all directories and remove those not matching the name
+        for file in *; do
+            if [ "$file" != "$capNameApi" ]; then
+                echo "clearing $file"
+                rm -rf "$file"
+            fi
+        done
+        popd || exit
+        # Run the specified api
+        echo "running $capNameApi"
+        ./api/"$capNameApi"
+    fi
+
+    # Check if the type is 'service'
+    if [ "$type" == "service" ]; then
+       local capNameService="${capName}Service"
+        echo "clearing service"
+        pushd services || exit
+        # Loop through all directories and remove those not matching the name
+        for file in *; do
+            if [ "$file" != "$capNameService" ]; then
+                echo "clearing $file"
+                rm -rf "$file"
+            fi
+        done
+        popd || exit
+        # Run the specified service
+        echo "running $capNameService"
+        ./services/"$capNameService"
+    fi
+}
+
+
+clear_and_run "$1" "$2"
