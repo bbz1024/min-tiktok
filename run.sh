8a0cbe299c08f363d67e3fceebf3763a49eb40da
bbz <2632141215@qq.com>
2024-07-14 16:41:45 +0800

    test: jenkins

--- run.sh
+++ run.sh
@@ -5,9 +5,22 @@ echo  "start build . . ."
 # build app
 echo "build app . . ."
 #docker build -t min-tiktok .
-docker save -o min-tiktok.tar min-tiktok
-
-scp min-tiktok.tar root@node2:/root/min-tiktok.tar
-ssh root@node2 "docker load -i /root/min-tiktok.tar"
+#docker save -o min-tiktok.tar min-tiktok
+#scp min-tiktok.tar root@node2:/root/min-tiktok.tar
+#ssh root@node2 "docker load -i /root/min-tiktok.tar"
 
 # collect conf
+echo "collect conf . . ."
+bash ./collect-etc.sh
+echo "collect conf done"
+cp ./k8s/nginx.conf /data/etc
+# scp
+echo "scp ..."
+ping -c 1 node2 || exit 1
+scp -r /data/etc root@node2:/data/etc || { echo "Failed to scp files"; exit 1; }
+echo "scp done ..."
+
+# k8s
+kubectl apply -f ./k8s/nginx.yaml
+kubectl apply -f ./k8s/auths.yaml
+kubectl apply -f ./k8s/user.yaml
