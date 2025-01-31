4db279b0b6badd460d7f59171d76b296fffd73f1
bbz <2632141215@qq.com>
2025-01-31 10:44:29 +0800

    test

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,6 @@ echo "building..."
 mkdir -p /output/apis
 mkdir -p /output/services
 mkdir -p /output/etc
-compress=${compress:-0}  # 使用默认值0，如果未定义或为空
 pushd api || exit
 for i in *;do
   echo "building $i"
@@ -12,9 +11,6 @@ for i in *;do
   capName="${name^}"
   cd "$i" || exit
   go build -ldflags="-s -w" -o "/output/apis/${capName}Api"
-  if [ "$compress" == 1 ]; then
-        /build/upx -9 "/output/apis/${capName}Api"
-  fi
   cp ./etc/* /output/etc/
   cd ..
 done
@@ -30,9 +26,6 @@ for i in *;do
   capName="${name^}"
   cd "$i" || exit
   go build -ldflags="-s -w" -o "/output/services/${capName}Service"
-  if [ "$compress" == 1 ]; then
-        /build/upx -9 "/output/services/${capName}Service"
-  fi
   cp ./etc/* /output/etc/
   cd ..
 done
